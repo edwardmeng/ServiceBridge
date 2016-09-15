c6155d34f28d5b80108c76daa2db0263eb980aa5
Edward Meng <edwardmeng@live.cn>
2016-09-15 23:58:47 +0800

    Rename nuget package names.

--- nuget/build.cmd
+++ nuget/build.cmd
@@ -19,53 +19,65 @@ set VisualStudioVersion=14.0
 REM Package restore
 echo.
 echo Running package restore...
-call :ExecuteCmd ..\tools\nuget.exe restore ..\Wheatech.ServiceModel.sln -OutputDirectory ..\packages -NonInteractive -ConfigFile nuget.config
+call :ExecuteCmd nuget.exe restore ..\ServiceBridge.sln -OutputDirectory ..\packages -NonInteractive -ConfigFile nuget.config
 IF %ERRORLEVEL% NEQ 0 goto error
 
 echo Building solution...
-call :ExecuteCmd %msbuild% "..\Wheatech.ServiceModel.sln" /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+call :ExecuteCmd %msbuild% "..\ServiceBridge.sln" /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 IF %ERRORLEVEL% NEQ 0 goto error
 
 echo Packaging...
 rem Core packages
-call :Package Wheatech.ServiceModel
-call :Package Wheatech.ServiceModel.Interception
-call :Package Wheatech.ServiceModel.Wcf
-call :Package Wheatech.ServiceModel.Mvc
-call :Package Wheatech.ServiceModel.Web
-call :Package Wheatech.ServiceModel.WebApi
+call :Package core\ServiceBridge ServiceBridge net40
+call :Package core\ServiceBridge.Interception ServiceBridge.Interception net40
 
 rem Integration packages
-call :Package Wheatech.ServiceModel.Autofac
-call :Package Wheatech.ServiceModel.Autofac.Interception
-call :Package Wheatech.ServiceModel.Ninject
-call :Package Wheatech.ServiceModel.Ninject.Interception
-call :Package Wheatech.ServiceModel.StructureMap
-call :Package Wheatech.ServiceModel.StructureMap.Interception
-call :Package Wheatech.ServiceModel.Unity
-call :Package Wheatech.ServiceModel.Unity.Interception
-call :Package Wheatech.ServiceModel.Windsor
-call :Package Wheatech.ServiceModel.Windsor.Interception
+call :Package WCF\ServiceBridge.ServiceModel ServiceBridge.ServiceModel net40
+call :Package WCF\ServiceBridge.ServiceModel.Activation ServiceBridge.ServiceModel.Activation net40
+call :Package Mvc\ServiceBridge.Mvc ServiceBridge.Mvc net45
+call :Package Mvc\ServiceBridge.Mvc.Activation ServiceBridge.Mvc.Activation net45
+call :Package Web\ServiceBridge.Web ServiceBridge.Web net45
+call :Package WebApi\ServiceBridge.WebApi ServiceBridge.WebApi net45
+call :Package WebApi\ServiceBridge.WebApi.Activation ServiceBridge.WebApi.Activation net45
+
+rem Implementation packages
+call :Package Autofac\ServiceBridge.Autofac ServiceBridge.Autofac net45
+call :Package Autofac\ServiceBridge.Autofac.Interception ServiceBridge.Autofac.Interception net45
+call :Package Autofac\ServiceBridge.Autofac.Activation ServiceBridge.Autofac.Activation net45
+call :Package Ninject\ServiceBridge.Ninject ServiceBridge.Ninject net40
+call :Package Ninject\ServiceBridge.Ninject.Interception ServiceBridge.Ninject.Interception net40
+call :Package Ninject\ServiceBridge.Ninject.Activation ServiceBridge.Ninject.Activation net40
+call :Package StructureMap\ServiceBridge.StructureMap ServiceBridge.StructureMap net45
+call :Package StructureMap\ServiceBridge.StructureMap.Interception ServiceBridge.StructureMap.Interception net45
+call :Package StructureMap\ServiceBridge.StructureMap.Activation ServiceBridge.StructureMap.Activation net45
+call :Package Unity\ServiceBridge.Unity ServiceBridge.Unity net45
+call :Package Unity\ServiceBridge.Unity.Interception ServiceBridge.Unity.Interception net45
+call :Package Unity\ServiceBridge.Unity.Activation ServiceBridge.Unity.Activation net45
+call :Package Windsor\ServiceBridge.Windsor ServiceBridge.Windsor net45
+call :Package Windsor\ServiceBridge.Windsor.Interception ServiceBridge.Windsor.Interception net45
+call :Package Windsor\ServiceBridge.Windsor.Activation ServiceBridge.Windsor.Activation net45
 
 goto end
 
 :Package
 set local
-set _PackageName=%1
+set _PackagePath=%1
+set _PackageName=%2
+set _Framework=%3
 set libtmp=%cd%\lib
 set packagestmp="%cd%\packages"
 set output=%cd%\..\output
 if not exist %libtmp% mkdir %libtmp%
 if not exist %packagestmp% mkdir %packagestmp%
-if not exist %libtmp%\net461 mkdir %libtmp%\net461
+if not exist %libtmp%\net461 mkdir %libtmp%\%_Framework%
 if not exist %output%\%config% mkdir %output%\%config%
 
-copy ..\src\%_PackageName%\bin\%config%\%_PackageName%.dll %libtmp%\net461\ /Y
-copy ..\src\%_PackageName%\bin\%config%\%_PackageName%.xml %libtmp%\net461\ /Y
-copy ..\src\%_PackageName%\bin\%config%\%_PackageName%.dll %output%\%config%\%_PackageName%.dll /Y
-copy ..\src\%_PackageName%\bin\%config%\%_PackageName%.xml %output%\%config%\%_PackageName%.xml /Y
+copy ..\src\%_PackagePath%\bin\%config%\%_PackageName%.dll %libtmp%\%_Framework%\ /Y
+copy ..\src\%_PackagePath%\bin\%config%\%_PackageName%.xml %libtmp%\%_Framework%\ /Y
+copy ..\src\%_PackagePath%\bin\%config%\%_PackageName%.dll %output%\%config%\%_PackageName%.dll /Y
+copy ..\src\%_PackagePath%\bin\%config%\%_PackageName%.xml %output%\%config%\%_PackageName%.xml /Y
 
-call :ExecuteCmd ..\tools\nuget.exe pack "%cd%\%_PackageName%.nuspec" -OutputDirectory %packagestmp% %version%
+call :ExecuteCmd nuget.exe pack "%cd%\%_PackageName%.nuspec" -OutputDirectory %packagestmp% %version%
 rmdir %libtmp% /S /Q
 exit /b %ERRORLEVEL%
 
